711a74c1fa9051db2cd3c8e946c55db3404c53d6
OsdaGomez99 <ogomez239@e.uneg.edu.ve>
2024-03-18 09:19:53 -0500

    Ultimo commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect('/ventas');
+});
+
 
 Route::resource('ventas', VentaController::class);
 Route::resource('proveedores', ProveedorController::class);
